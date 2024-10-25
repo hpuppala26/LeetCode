3999c98269c573c2797fc4c9fcc819dcd447681e
hpuppala26 <146597944+hpuppala26@users.noreply.github.com>
2024-10-24 23:55:47 -0700

    Update Product_Of_Array_Except_Self.py

--- Product_Of_Array_Except_Self.py
+++ Product_Of_Array_Except_Self.py
@@ -1,1 +1,14 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        prefix = [1]*len(nums)
+        postfix = [1]*len(nums)
+        prefix[0] = nums[0]
+        postfix[len(nums)-1]=nums[-1]
+        for i in range(1,len(nums)):
+            prefix[i] = nums[i]*prefix[i-1]
+        for j in range(len(nums)-2,-1,-1):
+            postfix[j] = nums[j]*postfix[j+1]
+        print(prefix, postfix)
 
+
+        
